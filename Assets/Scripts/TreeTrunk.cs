6f5bbe0f0efb50c63c1d4946a9cb169ea3693343
lolicon77 <huangforbusi@foxmail.com>
2016-11-18 17:47:04 +0800

    ~

---
+++ Assets/Scripts/TreeTrunk.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Random = UnityEngine.Random;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[Serializable]
+public struct Range {
+	public float min;
+	public float max;
+}
+
+public class TreeTrunk : MonoBehaviour {
+
+	public float initialTreeRadius;
+	public int initialTreeSubdivisions;
+	public Range initialTreeRadiusRange;
+
+	private Mesh mesh;
+
+	void Awake() {
+		mesh = new Mesh();
+	}
+
+	void InitBottom() {
+		var realRadius = initialTreeRadius * Random.Range(initialTreeRadiusRange.min, initialTreeRadiusRange.max);
+	}
+
+
+
+}
+
